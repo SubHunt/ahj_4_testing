12489b676d6e5e6f941983e4b51f62d8aae86f53
SubHunt <phaeton.kz@gmail.com>
2024-04-18 01:03:50 +0300

    try Git pages actions 3

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,6 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
-    library: 'ajs', // название нашей библиотеки
-    libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
-    libraryExport: 'default', // экспортируется имя default
-    globalObject: 'this', // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
   },
   mode: 'development',
   module: {
