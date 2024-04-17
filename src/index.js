518f3b81f2a670210ed452932cc233fad6b239cf
SubHunt <phaeton.kz@gmail.com>
2024-04-18 00:23:44 +0300

    try Git pages actions 2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import "./css/style.css";
 import "./js/app";
 
 import '../licenses.txt';
+
+function info() {
+    const m = new Map();
+    m.set('name', 'ajs');
+    m.set('version', '1.0.0');
+    return m;
+  }
+  
+  export default {
+    info
+  };
